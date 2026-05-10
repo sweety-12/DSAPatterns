92942fd51a55362b6c75121496335f4a7623e937
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-05-10 11:42:45 +0530

    Update tripletWithSmallerSum.cpp

--- 2PointerApproach/tripletWithSmallerSum.cpp
+++ 2PointerApproach/tripletWithSmallerSum.cpp
@@ -37,3 +37,40 @@ int count_triplets(vector<int>& nums, int target) {
     }
     return count;
 }
+
+
+The Logic for :   
+Count += right-left 
+
+nums = [-1, 0, 2, 3, 4], target = 6
+        i   L          R
+
+nums[i] + nums[L] + nums[R] < target
+-1      +  0      +  4      = 3 < 6 ✅
+Since array is sorted — everything between L and R is smaller than R.
+So ask yourself:
+
+If -1 + 0 + 4 < 6
+then -1 + 0 + 3 < 6 ?
+
+Yes — because 3 < 4 ✅
+
+then -1 + 0 + 2 < 6 ?
+
+Yes — because 2 < 4 ✅
+
+So how many valid right pointers exist at this moment?
+L         R
+0    2    3    4
+     ↑    ↑    ↑
+     R1   R2   R3   →  3 valid rights for this L
+How many elements are between L and R?
+right - left = 4 - 1 = 3 ✅
+
+One line logic
+
+"If smallest + largest < target, then smallest + anything in between < target too."
+
+That "anything in between" is exactly right - left elements.
+
+                                        "Count a range at once instead of one by one"
