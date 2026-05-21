23314f3521c724b9fe3d0755be7c6480d25e2c1f
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-05-21 14:13:26 +0530

    Update cycleDetectionInLinkedList.cpp

--- 2PointerApproach/cycleDetectionInLinkedList.cpp
+++ 2PointerApproach/cycleDetectionInLinkedList.cpp
@@ -39,3 +39,81 @@ bool detectCycle(Node * head){
 
     return false;
 }
+
+Now you may be having a question, Storing nodes in set, what if the elements are repeated. Will this solution still works so yes it will work, 
+coz storing node * means we are storing a address of that node not data/value.
+
+What You Are Storing
+
+You are storing:
+
+Node*
+
+which means:
+
+memory addresses of nodes
+
+NOT node values.
+
+Example
+
+Suppose linked list is:
+
+1 -> 2 -> 1 -> NULL
+
+Here values repeat:
+
+1 appears twice
+
+BUT these are different nodes in memory.
+
+Memory View
+Node A -> val = 1
+Node B -> val = 2
+Node C -> val = 1
+
+Addresses:
+
+A = 0x100
+B = 0x200
+C = 0x300
+
+Even though values are same:
+
+A != C
+
+because addresses differ.
+
+So This Is SAFE
+unordered_set<Node*> st;
+
+because set compares pointers (addresses).
+
+This Would Be WRONG
+unordered_set<int> st;
+
+////////////////////////////////////////////////////////////
+
+OPTIMIZED APPROACH(Floyd's Algorithm) :
+TC : O(n) and SC : O(1)
+
+bool detectCycle(Node* head){
+
+    //edge case
+    if(head == NULL || head ->next == NULL)  return false;  /
+
+    Node * slow = head;
+    Node * fast = head;
+
+    while( fast != NULL && fast->next != NULL){
+
+        fast = fast->next->next;
+        slow = slow->next;
+        
+        if(slow == fast) return true;
+        
+    }
+
+    return false;
+}
+
