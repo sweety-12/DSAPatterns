1da3a3a9df22b42340aa77fe790c107ed43886a7
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-06-17 21:21:00 +0530

    Create bookAllocation.cpp

---
+++ BinarySearch/bookAllocation.cpp
@@ -0,0 +1,94 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                **EXTREMELY HARD BINARY SEARCH QUESTION***
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Problem statement
+Given an array ‘arr’ of integer numbers, ‘arr[i]’ represents the number of pages in the ‘i-th’ book.
+There are ‘m’ number of students, and the task is to allocate all the books to the students.
+Allocate books in such a way that:
+
+1. Each student gets at least one book.
+2. Each book should be allocated to only one student.
+3. Book allocation should be in a contiguous manner.
+You have to allocate the book to ‘m’ students such that the maximum number of pages assigned to a student is minimum.
+If the allocation of books is not possible, return -1.
+Example:
+Input: ‘n’ = 4 ‘m’ = 2 
+‘arr’ = [12, 34, 67, 90]
+
+Output: 113
+
+BRUTE FORCE : Linear Search
+  TC : O(N * (sum(arr[])-max(arr[])+1)) And SC :O(1)
+
+  int countStudents(vector<int> &arr, int pages) {
+    int n = arr.size(); //size of array.
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            //add pages to current student
+            pagesStudent += arr[i];
+        }
+        else {
+            //add pages to next student
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    //book allocation impossible:
+    if (m > n) return -1;
+
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+
+    for (int pages = low; pages <= high; pages++) {
+        if (countStudents(arr, pages) == m) {
+            return pages;
+        }
+    }
+
+OPTIMIZED APPROACH:
+TC : O(N * log(sum(arr[])-max(arr[])+1)) and SC : O(1)
+
+int countStudents(vector<int> &arr, int pages) {
+    int n = arr.size(); //size of array.
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            //add pages to current student
+            pagesStudent += arr[i];
+        }
+        else {
+            //add pages to next student
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    //book allocation impossible:
+    if (m > n) return -1;
+
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int students = countStudents(arr, mid);
+        if (students > m) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+    return low;
+}
