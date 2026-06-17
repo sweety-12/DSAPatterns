1bf6cb887dc64b70ae7c8369182d938c87df0485
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-06-17 19:58:45 +0530

    Update AggressiveCows.cpp

--- BinarySearch/AggressiveCows.cpp
+++ BinarySearch/AggressiveCows.cpp
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                        ***BINARY SEARCH HARD***
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 SOLVED IN GFG - You are given an array with unique elements of stalls[], which denote the positions of stalls. You are also given an integer k 
 which denotes the number of aggressive cows. The task is to assign stalls to k cows such that the minimum distance between any two of them 
 is the maximum possible.
