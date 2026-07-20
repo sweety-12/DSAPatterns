128fa48674d95484f500aaa1d9f594b0739a42e9
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-07-20 23:31:45 +0530

    Add solution for Partition Equal Subset Sum problem
    Implement recursive and memoization approaches for partitioning an array into two subsets with equal sum.

---
+++ Dynamic_Programming/Parition_equal_subset_sum.cpp
@@ -0,0 +1,113 @@
+416. Partition Equal Subset Sum
+Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+RECURSIVE APPROACH:
+TC : O(2^n) and SC : O(n)
+
+class Solution {
+public:
+    bool func(int ind, int target, vector<int>&nums){
+
+        //base case
+        if(target == 0) return true;
+
+        if(ind == 0){
+            if(nums[0] == target) return true;
+
+            else return false;
+        }
+
+        //recursive function
+        bool not_taken = func(ind -1, target, nums);
+
+        bool taken = false;
+        if(nums[ind] <= target){
+            taken = func(ind-1, target - nums[ind], nums);
+        }
+
+        return taken | not_taken;
+    }
+    bool canPartition(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int target =0;
+        int sum =0;
+
+        for(int i=0; i<n; i++){
+             sum += nums[i];
+        }
+
+        if(sum % 2 != 0)  return false; // odd summation can never be divided equally
+
+        target = sum/2;
+
+        return func(n-1, target, nums);
+
+        
+    }
+};
+
+MEMOIZATION APPROACH:
+TC: O(n* target) and SC : O(n * target) + O(n)
+
+class Solution {
+public:
+    bool func(int ind, int target, vector<int>&nums, vector<vector<int>>&dp){
+
+        //base case
+        if(target == 0) return true;
+
+        if(ind == 0){
+            if(nums[0] == target) return true;
+
+            else return false;
+        }
+
+        //checking for overlapping subproblem
+        if(dp[ind][target] != -1)  return dp[ind][target];
+
+        //recursive function
+        bool not_taken = func(ind -1, target, nums, dp);
+
+        bool taken = false;
+        if(nums[ind] <= target){
+            taken = func(ind-1, target - nums[ind], nums, dp);
+        }
+
+        return dp[ind][target] = taken | not_taken;
+    }
+    bool canPartition(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int target =0;
+        int sum =0;
+
+        for(int i=0; i<n; i++){
+             sum += nums[i];
+        }
+
+        if(sum % 2 != 0)  return false; // odd summation can never be divided equally
+
+        target = sum/2;
+
+
+        //converting into memoization
+        vector<vector<int>>dp(n, vector<int>(target + 1, -1));
+
+        return func(n-1, target, nums, dp);
+
+        
+    }
+};
