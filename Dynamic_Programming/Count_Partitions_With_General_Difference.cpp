3d8ad338f2eb3e465ce99d56e1a504da11edc258
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-07-25 23:08:21 +0530

    Added tabulation approach

--- Dynamic_Programming/Count_Partitions_With_General_Difference.cpp
+++ Dynamic_Programming/Count_Partitions_With_General_Difference.cpp
@@ -43,7 +43,7 @@ int func(int ind, int target, vector<int>&arr){
         }
         
         //edge case -- OK THIS IS ONE OF THE EDGE CASE 
-        //WE NEED TO TAKE CARE OF COZ (SUM - DIFF) /2 MUST BE AN POSITIVE
+        //WE NEED TO TAKE CARE OF COZ (SUM - DIFF) /2 MUST BE A POSITIVE
         //INTERGER AS THIS NEEDS TO BE IN THE REMAINING ARRAY
         if((sum - diff)%2 != 0) return 0;
         
@@ -53,7 +53,7 @@ int func(int ind, int target, vector<int>&arr){
     }
 
 MEMOIZATION APPRAOCH:
-TC : O(n*diff) and SC : O(n*diff)
+TC : O(n*target) and SC : O(n*target)
 
    int func(int ind, int target, vector<int>&arr, vector<vector<int>>&dp){
         
@@ -111,4 +111,58 @@ TC : O(n*diff) and SC : O(n*diff)
         vector<vector<int>>dp(n, vector<int>(target + 1, -1));
         
         return func(n-1, target, arr, dp);
+    }
+
+TABULATION APPROACH;
+TC: O(N*TARGET) AND SC : O(N*TARGET)
+
+ int tabulationApproach(vector<int>&arr, int n, int diff, int target,  vector<vector<int>>&dp){
+        
+        //base case
+        //THIS BASE CASE IS HAUNTING ME BROTHER...... :(
+        if(arr[0] == 0)  dp[0][0] = 2;
+        else dp[0][0] =1;
+        
+        if(arr[0] != 0 && arr[0] <= target)  dp[0][arr[0]] = 1;
+        
+        //nested loop
+        for(int i = 1; i<n; i++){
+            for (int j = 0 ; j<= target ; j++){
+                
+                int not_pick = dp[i-1][j];
+                
+                int pick = 0;
+                if(arr[i] <= j){
+                    pick = dp[i-1][j- arr[i]];
+                }
+                
+                dp[i][j] = pick + not_pick;
+                
+            }
+        }
+        
+        return dp[n-1][target];
+    }
+    int countPartitions(vector<int>& arr, int diff) {
+        // Code here
+        int target =0;
+        int n = arr.size();
+        
+        int sum =0;
+        for(int i=0; i<n ; i++){
+            sum += arr[i];
+        }
+        
+        //edge case -- OK THIS IS ONE OF THE EDGE CASE 
+        //WE NEED TO TAKE CARE OF COZ (SUM - DIFF) /2 MUST NOT BE A DECIMAL VALUE 
+        //AND BE AN POSITIVE INTERGER AS THIS NEEDS TO BE IN THE ARRAY
+        
+        if((sum - diff) %2 != 0 || (sum - diff) < 0) return 0; 
+        
+        target = (sum-diff)/2;   //THE TARGET WE ARE CALCULATING MUST ALSO BE POSITIVE
+        
+        vector<vector<int>>dp(n, vector<int>(target + 1, 0));
+        
+        return tabulationApproach(arr, n, diff, target, dp);
+        
     }
