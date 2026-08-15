acabc04492e559b4192e20b258737e602213563f
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-08-15 12:46:52 +0530

    Add tabulation approach for LCS in string conversion
    Implemented tabulation approach for longest common subsequence.

--- Dynamic_Programming/Minimum_insertions_deletions_to_convert_String_A_to_B.cpp
+++ Dynamic_Programming/Minimum_insertions_deletions_to_convert_String_A_to_B.cpp
@@ -92,3 +92,40 @@ TC: O(n*m) and SC : O(n*m) + O(n+m)
 
        return (n + m) - 2*longestCommonSubsequence(word1, word2);
     }
+
+TABULATION APPROACH:
+TC: O(n*m) and SC : O(n*m)
+
+int longestCommonSubsequence(string text1, string text2) {
+
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+
+        //BASE CASE
+        for(int i =0; i<=m; i++) dp[0][i] = 0;
+        for(int i =0; i<=n; i++) dp[i][0] = 0;
+
+
+        for(int ind1 = 1; ind1<= n; ind1++){
+            for(int ind2 = 1; ind2<=m; ind2++){
+
+                if(text1[ind1-1] == text2[ind2-1]){
+
+                  dp[ind1][ind2] =  1 + dp[ind1 -1][ind2 -1];
+                }
+
+                else dp[ind1][ind2] =  0 + max( dp[ind1 -1][ind2], dp[ind1][ind2-1]);
+            }
+        }
+        
+        return dp[n][m];
+    }
+    int minDistance(string word1, string word2) {
+        
+       int n = word1.length();
+       int m = word2.length();
+
+       return (n + m) - 2*longestCommonSubsequence(word1, word2);
+    }
