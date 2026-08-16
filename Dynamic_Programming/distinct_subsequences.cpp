f77ca9fe3fae1208a8bf03fa5cffd04e7ca1b4cc
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-08-16 22:13:28 +0530

    Implemented Tabulation approach and Change DP table to double to prevent overflow
    Updated the numDistinct function to use a double type for the DP table to avoid integer overflow issues. Adjusted initialization of the DP table and clarified comments.

--- Dynamic_Programming/distinct_subsequences.cpp
+++ Dynamic_Programming/distinct_subsequences.cpp
@@ -90,3 +90,43 @@ public:
         return func(n-1, m-1, s, t, dp);
     }
 };
+
+
+TABULATION APPROACH:
+TC: O(n*m) and SC : O(n*m)
+
+// HERE IN TABULATION , WAS GETTING INTERGER OVERFLOW, LONG LONG ALSO WASN'T WORKING. LASTLY DOUBLE WORKED.
+  
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        
+        int n = s.length();
+        int m = t.length();
+
+        vector<vector<double>>dp(n+1, vector<double >(m+1, 0));   //<- HERE DOUBLE
+        //base case
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        for(int j =1; j<=m; j++){   //<- this can be omitted as dp table is already intiliazed with 0 
+            dp[0][j] = 0;
+        }
+
+        //nested loop
+        for(int i =1; i<= n; i++){
+            for(int j =1; j<= m; j++){
+
+            if(s[i-1] == t[j-1]){
+            //take
+             dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            }
+        //not take
+             else dp[i][j] = dp[i-1][j];
+
+            }
+        }
+
+        return dp[n][m];
+    }
+};
