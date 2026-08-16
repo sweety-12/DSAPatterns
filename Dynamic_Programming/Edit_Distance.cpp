98d407b1a29faa61d473f89892816f5c29ecac63
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-08-16 23:18:15 +0530

    Add memoization to edit distance function
    Implemented memoization approach for edit distance calculation.

--- Dynamic_Programming/Edit_Distance.cpp
+++ Dynamic_Programming/Edit_Distance.cpp
@@ -59,3 +59,36 @@ public:
         return func(n-1, m-1, s1, s2);
     }
 };
+
+MEMOIZATION APPROACH:
+TC: O(n*m) and SC : O(n*m) + O(n+m)
+
+class Solution {
+public:
+    int func(int i, int j, string &s1, string &s2, vector<vector<int>>&dp){
+
+        //base case
+        if(i<0) return j+1;   //if i <0 then we need i+1 insertion to s1
+        if(j<0) return i+1;   //if j <0 then we need i+1 deletion from s1
+
+        //overlapping sub problem
+        if(dp[i][j] != -1)  return dp[i][j];
+
+
+        //recursive call
+        if(s1[i] == s2[j]){
+            return dp[i][j] =  0 + func(i-1, j-1, s1, s2, dp);
+        }
+
+        return dp[i][j] = 1 + min(func(i-1, j-1, s1, s2, dp), min(func(i-1, j, s1, s2, dp), func(i, j-1, s1, s2, dp)));
+
+    }
+    int minDistance(string s1, string s2) {
+        
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+        return func(n-1, m-1, s1, s2, dp);
+    }
+};
