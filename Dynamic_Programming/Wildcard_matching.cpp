030dbc3a50957274896f0b2b9e63b67ae8cbacd3
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-08-17 09:08:12 +0530

    Add Wildcard Matching solution in C++ recursive approach
    Implement wildcard pattern matching with '?' and '*' support.

---
+++ Dynamic_Programming/Wildcard_matching.cpp
@@ -0,0 +1,74 @@
+//MY UNDERSTANDING FOR THIS QUESTION : 3 scenarios 
+// 1. if s[i] == p[j] || s[i] = '?' simply do i-1, j-1
+// 2. if s[i] == '*'  this represents sequence of characters that can be matched , first either we can skip the current character i.e (i-1, j) or take it i.e (i, j-1). Check out the recursive tree if forgot
+// 3. if charcters does not match simply return false
+
+//AMAZING QUESTION IT WAS. AS THIS IS THE LAST QUESTION FROM DP ON STRINGS. I CAN SAY HARD QUESTIONS NEEDS MODIFICATION ON RECURSIVE CALL VALA PART, ELSE IF IT IS MEDIUM LEVEL QUESTION, BARELY ANY CHANGES IN
+//THE RECURSIVE PART, IT ONLY CHANGES IN BASE CASE PART
+
+LEETCODE 44. Wildcard Matching
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+ 
+
+Example 1:
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input: s = "aa", p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+Example 3:
+
+Input: s = "cb", p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+
+RECURSIVE CALL
+TC: O(2^n * 2*m) and SC : O(n+m)
+
+class Solution {
+public:
+    bool func(int i, int j, string &s, string &p){
+        //base case
+        if(i<0 && j <0) return true;
+        if(i>= 0 && j < 0)  return false;
+        if(i<0 && j>=0) {
+            for(int temp = 0; temp <= j; temp++){
+                if(p[temp] != '*') return false;
+            }
+
+            return true;
+        }
+
+        //recursive call
+        //matches
+        if(s[i] == p[j] || p[j] == '?'){
+            return func(i-1, j-1, s, p);
+        }
+
+        //if having *
+        if(p[j] == '*'){
+            return func(i-1, j, s, p) || func(i, j-1, s, p);
+        }
+
+        //else case i.e s[i] != p[j]
+        return false;
+    }
+    bool isMatch(string s, string p) {
+
+        int n = s.size();
+        int m = p.size();
+
+        return func(n-1, m-1, s, p);
+        
+    }
+};
+
