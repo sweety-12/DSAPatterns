c82184e3b6b4e7aa50ae9981ffc664e7a24711dd
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-08-17 09:09:27 +0530

    Add memoization method for wildcard matching
    Implemented a memoization approach for wildcard matching.

--- Dynamic_Programming/Wildcard_matching.cpp
+++ Dynamic_Programming/Wildcard_matching.cpp
@@ -72,3 +72,47 @@ public:
     }
 };
 
+MEMOIZATION APPROACH:
+TC: O(n*m) and SC : O(n*m) + O(n+m)
+
+class Solution {
+public:
+    bool func(int i, int j, string &s, string &p,  vector<vector<int>>&dp){
+        //base case
+        if(i<0 && j <0) return true;
+        if(i>= 0 && j < 0)  return false;
+        if(i<0 && j>=0) {
+            for(int temp = 0; temp <= j; temp++){
+                if(p[temp] != '*') return false;
+            }
+
+            return true;
+        }
+
+        //overlapping subproblm
+        if(dp[i][j] != -1)  return dp[i][j];
+
+        //recursive call
+        //matches
+        if(s[i] == p[j] || p[j] == '?'){
+            return dp[i][j] = func(i-1, j-1, s, p, dp);
+        }
+
+        //if having *
+        if(p[j] == '*'){
+            return dp[i][j] =  func(i-1, j, s, p, dp) || func(i, j-1, s, p, dp);
+        }
+
+        //ekse case i.e s[i] != p[j]
+        return dp[i][j] =  false;
+    }
+    bool isMatch(string s, string p) {
+
+        int n = s.size();
+        int m = p.size();
+
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+        return func(n-1, m-1, s, p, dp);
+        
+    }
+};
