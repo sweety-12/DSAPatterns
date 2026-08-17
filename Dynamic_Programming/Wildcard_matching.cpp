ead55857317d4679a8dc94934a7157115e24edb3
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-08-17 09:32:41 +0530

    Add tabulation method for wildcard matching
    Implement tabulation approach for wildcard matching.

--- Dynamic_Programming/Wildcard_matching.cpp
+++ Dynamic_Programming/Wildcard_matching.cpp
@@ -116,3 +116,54 @@ public:
         
     }
 };
+
+
+TABULATION APPROACH:
+TC: O(n*m) and SC : O(n*m)
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+
+        int n = s.size();
+        int m = p.size();
+
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+
+        dp[0][0] = true;
+        for(int i =0; i<=n; i++){
+            dp[i][0] = false;
+        }
+
+        for(int j =0; j<=m ; j++){
+            int flag = true;
+              for(int temp = 1; temp <= j; temp++){
+                        if(p[temp-1] != '*') 
+                           flag= false;
+              }
+
+                dp[0][j] = flag;
+        }
+        
+
+        //nested loop
+        for(int i=1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
+                dp[i][j] = dp[i-1][j-1];
+                }
+
+                //if having *
+                else if(p[j-1] == '*'){
+                dp[i][j] =  dp[i-1][j] || dp[i][j-1];
+                }
+
+                //else case i.e s[i] != p[j]
+                else dp[i][j] =  false;
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
