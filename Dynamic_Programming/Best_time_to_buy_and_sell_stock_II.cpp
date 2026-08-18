59540d0bc409a46be88e8ebe8fc58d911aa6f500
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-08-19 00:10:03 +0530

    Add tabulation method for stock trading max profit
    Implemented tabulation approach for max profit calculation in stock trading problem.

--- Dynamic_Programming/Best_time_to_buy_and_sell_stock_II.cpp
+++ Dynamic_Programming/Best_time_to_buy_and_sell_stock_II.cpp
@@ -97,3 +97,37 @@ public:
         
     }
 };
+
+TABULATION APPROACH:
+TC: O(n*m) and SC : O(n*m)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n = prices.size();
+
+        vector<vector<long>>dp(n+1, vector<long>(2, 0));
+
+        dp[n][0] = dp[n][1] = 0;
+
+        for( int ind= n-1; ind >= 0; ind--){
+            for(int buy = 0; buy <= 1; buy++){
+
+                long profit = 0;
+
+                if(buy){
+                    profit = max(- prices[ind] + dp[ind+1][0] , 0 + dp[ind+1][1]);
+                }
+
+                else{
+                    profit = max(prices[ind] + dp[ind+1][1], 0 + dp[ind+1][0]);
+                }
+
+                dp[ind][buy] = profit;
+            }
+        }
+
+            return dp[0][1];        
+    }
+};
