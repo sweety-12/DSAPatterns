caaa8b46d313f11638465473ae57ca3b3e91cdda
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-03 08:28:19 +0530

    Add memoization to best_time_to_buy_and_sell_stock_III
    Implemented memoization approach for stock trading problem.

--- Dynamic_Programming/best_time_to_buy_and_sell_stock_III.cpp
+++ Dynamic_Programming/best_time_to_buy_and_sell_stock_III.cpp
@@ -38,3 +38,35 @@ public:
         return func(0, 1, 2, prices, n);
     }
 };
+
+MEMOIZATION APPROACH :
+TC : O(n * 2 * 3) and SC : O (n* 2* 3)
+
+class Solution {
+public:
+    int func(int ind, int buy, int cap, vector<int>& prices, int n,  vector<vector<vector<int>>>&dp){
+         //base case 
+        if(cap == 0)  return 0;
+        if(ind == n)  return 0;
+
+        //overlapping sub problem
+        if(dp[ind][buy][cap] != -1)  return dp[ind][buy][cap];
+
+         //recurrsive calls
+         if(buy == 0){
+            return dp[ind][buy][cap] = max( prices[ind] + func(ind+1, 1, cap-1, prices, n, dp), 
+                         0 + func(ind +1, 0, cap, prices, n, dp));
+         }
+
+         return dp[ind][buy][cap] = max (-prices[ind] + func(ind +1, 0, cap, prices,  n, dp),
+                        0 + func(ind +1, 1, cap, prices, n, dp));
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        
+        //memoization
+        //dp of size n*2*3
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        return func(0, 1, 2, prices, n, dp);
+    }
+};
