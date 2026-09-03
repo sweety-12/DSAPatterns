6569935708a47a13b07b8bed01a1ab61105b6a2a
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-03 08:32:34 +0530

    Update best_time_to_buy_and_sell_stock_III.cpp

--- Dynamic_Programming/best_time_to_buy_and_sell_stock_III.cpp
+++ Dynamic_Programming/best_time_to_buy_and_sell_stock_III.cpp
@@ -124,3 +124,42 @@ public:
         return dp[0][1][2];
     }
 };
+
+SPACE OPTIMIZATION:
+TC : O(n*2*3) and SC : O(1)  <- TWO VECTORS OF SIZE 2*3 WHICH MEANS ALMOST CONSTANT.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        
+        //memoization
+        //dp of size n*2*3
+
+        //SPACE OPTIMIZATION
+        vector<vector<int>>  after(2, vector<int>(3, 0));
+        vector<vector<int>>  curr(2, vector<int>(3, 0));
+
+
+        //NESTED LOOPS
+        for(int ind = n-1 ; ind >= 0; ind--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+
+
+                     if(buy == 0){
+                        curr[buy][cap] = max( prices[ind] + after[1][cap-1], 0 + after[0][cap]);
+                    }
+
+                    else {
+                       curr[buy][cap] = max (-prices[ind] + after[0][cap], 0 + after[1][cap]);
+
+                       } 
+                }
+            }
+
+            after = curr;
+        }
+        return after[1][2];
+    }
+};
