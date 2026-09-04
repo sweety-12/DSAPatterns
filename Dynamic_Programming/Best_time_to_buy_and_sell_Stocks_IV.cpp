9ff6c47cb1552387686e84415109e7d6a0d25a33
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-04 07:40:16 +0530

    Optimize maxProfit function using space reduction
    Implemented space optimization for maxProfit function in Best_time_to_buy_and_sell_Stocks_IV.cpp.

--- Dynamic_Programming/Best_time_to_buy_and_sell_Stocks_IV.cpp
+++ Dynamic_Programming/Best_time_to_buy_and_sell_Stocks_IV.cpp
@@ -109,3 +109,40 @@ public:
         return dp[0][1][k];
     }
 };
+
+Space Optimizatio Approach:
+TC : O(n*2*k) and SC : O(1)
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        
+        int n = prices.size();
+
+        //Space optmization
+        vector<vector<int>> after(2, vector<int>(k+1, 0));
+        vector<vector<int>> curr(2, vector<int>(k+1, 0));
+
+
+        //nested loops
+        for(int ind = n-1; ind >= 0 ; ind--){
+            for(int buy = 0 ; buy <= 1; buy ++){
+                for(int cap = 1; cap <= k; cap++){
+                     if(buy == 1){
+                        curr[buy][cap] =  max(-prices[ind] + after[0][cap], 
+                                        0 + after[1][cap]);  
+                    }
+
+                    else{
+                        curr[buy][cap] = max (prices[ind] + after[1][cap-1], 
+                                0 + after[0][cap]); 
+                    }  
+                }
+            }
+
+            after = curr;
+        }
+
+        return after[1][k];
+    }
+};
