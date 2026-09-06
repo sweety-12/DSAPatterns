6380b67abae7be54e72634791190dd15abdaff25
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-06 23:11:55 +0530

    Optimize maxProfit by removing nested loop
    Removed the nested loop to optimize the algorithm. Time complexity is now O(n).

--- Dynamic_Programming/Best_time_to_buy_and_Sell_stock_with_cooldown.cpp
+++ Dynamic_Programming/Best_time_to_buy_and_Sell_stock_with_cooldown.cpp
@@ -109,3 +109,37 @@ public:
         return dp[0][1];
     }
 };
+
+
+REMOVED THE NESTED LOOP:
+TC: O(n)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n = prices.size();
+
+        vector<vector<int>>dp(n+2, vector<int>(2, 0));
+
+        //nested loop
+        for(int ind =n-1; ind >=0; ind--){
+            // for(int buy =0; buy <= 1; buy++){
+
+                //copy the recurrence
+                //  if(buy == 1){
+                     dp[ind][1] = max(-prices[ind] + dp[ind + 1][0],
+                                    0    + dp[ind + 1][1]);
+                //   }
+
+                //    else{
+                     dp[ind][0] = max(prices[ind] +dp[ind + 2][1], 
+                                0      + dp[ind + 1][0]);
+                //    }  
+                //  }
+
+        }
+        
+        return dp[0][1];
+    }
+};
