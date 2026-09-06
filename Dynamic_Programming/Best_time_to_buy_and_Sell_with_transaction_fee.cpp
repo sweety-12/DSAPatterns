f51d2b40aa50d4fbe5777114e0b30e626f710799
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-06 23:15:02 +0530

    Add tabulation and space optimization for maxProfit
    Implemented tabulation and space optimization approaches for max profit calculation with transaction fee.

--- Dynamic_Programming/Best_time_to_buy_and_Sell_with_transaction_fee.cpp
+++ Dynamic_Programming/Best_time_to_buy_and_Sell_with_transaction_fee.cpp
@@ -80,4 +80,63 @@ public:
 };
 
 
+TABULATION APPROACH:
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        
+        int n = prices.size();
+
+        vector<vector<int>>dp(n+1, vector<int>(2, 0));
+
+
+        for(int ind = n-1; ind >=0; ind--){
+            for(int buy = 0; buy <= 1; buy++){
+                if(buy == 1){
+                    dp[ind][buy] = max(-prices[ind] + dp[ind + 1][0], 
+                                    0       + dp[ind + 1][1]);
+                }
+
+                else{
+                    dp[ind][buy]  = max((prices[ind] - fee) + dp[ind + 1][1], 
+                                0              +  dp[ind + 1][0]);
+                    }
+                }   
+        }
+        return dp[0][1];
+    }
+};
+
+SPACE OPTIMIZATION:
+
+TC: O(n*2) and SC: O (1)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        
+        int n = prices.size();
+
+        vector<int>after(2, 0);
+        vector<int>curr(2, 0);
+
+
+        for(int ind = n-1; ind >=0; ind--){
+            for(int buy = 0; buy <= 1; buy++){
+                if(buy == 1){
+                    curr[buy] = max(-prices[ind] + after[0], 
+                                    0       + after[1]);
+                }
+
+                else{
+                   curr[buy]  = max((prices[ind] - fee) + after[1], 
+                                0              +  after[0]);
+                    }
+                }
+
+                after = curr;   
+        }
+        return after[1];
+    }
+};
  
