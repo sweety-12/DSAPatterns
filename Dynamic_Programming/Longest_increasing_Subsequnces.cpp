ee979abafe50cf404bb3ff172944d24d42410730
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-10 07:14:13 +0530

    Add space-optimized LIS solution in C++
    Implemented space-optimized solution for finding the length of the longest increasing subsequence using dynamic programming.

--- Dynamic_Programming/Longest_increasing_Subsequnces.cpp
+++ Dynamic_Programming/Longest_increasing_Subsequnces.cpp
@@ -113,3 +113,38 @@ public:
         return dp[0][-1+1];
     }
 };
+
+Space Optimization:
+TC: O(n*n) and SC : O(n) * 2 
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> next (n+1, 0);
+        vector<int> curr (n+1, 0);
+
+        //base case not needed coz dp array anyways returning 0
+        //nested loop
+        for(int ind = n-1; ind >=0; ind--){
+            for(int prev = ind -1; prev >= -1; prev--){
+
+                //not take
+                 int len =  0 + next[prev+1];
+
+                //take 
+                if(prev == -1 || nums[ind] > nums[prev]){
+                    
+                    len =  max(len,  1 + next[ind+1]);
+                }
+
+                 curr[prev + 1] = len;
+            }
+
+            next = curr;
+        }
+
+        return next[-1+1];  // <- this +1 thing is index shifting
+    }
+};
