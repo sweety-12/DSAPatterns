f1d7673fa2b3bedbc18a2cca406891f0e3daaf1d
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-16 22:29:14 +0530

    Add tabulation approach for minimum cost stick cutting
    Implemented a tabulation approach for calculating the minimum cost to cut a stick, improving efficiency with a time complexity of O(c^3) and space complexity of O(c^2).

--- Dynamic_Programming/minimum_cost_to_cut_a_stick.cpp
+++ Dynamic_Programming/minimum_cost_to_cut_a_stick.cpp
@@ -99,3 +99,42 @@ public:
     }
 };
 
+TABULATION APPROACH:
+TC: O(c^3) AND SC: O(c^2)
+
+class Solution {
+public:
+   
+    int minCost(int lenOfStick, vector<int>& cuts) {
+
+        int c = cuts.size();
+
+        cuts.push_back(lenOfStick);
+        cuts.insert(cuts.begin(), 0);
+
+        sort(cuts.begin(), cuts.end());
+
+        vector<vector<int>>dp(c+2, vector<int>(c+2, 0));
+
+        for(int i = c; i>= 1; i--){
+
+            for(int j = i; j <= c; j++){
+
+                int mini = INT_MAX;
+
+
+                for(int ind = i; ind <= j; ind++){
+
+                    int ans = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind + 1][j];
+
+
+                    mini = min(mini, ans);
+                }
+
+                dp[i][j] = mini;
+            }
+        }
+
+        return dp[1][c];
+    }
+};
