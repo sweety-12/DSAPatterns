c200803fe6187aa60123c3518465cd056984d0e9
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-18 00:06:08 +0530

    Implement tabulation approach for burst balloons problem
    Added a tabulation approach for the burst balloons problem with time complexity O(n^3) and space complexity O(n^2).

--- Dynamic_Programming/burst_balloons.cpp
+++ Dynamic_Programming/burst_balloons.cpp
@@ -54,7 +54,7 @@ public:
 
 
 MEMOIZATION APPROACH:
-TC: O(n^3) and SC : O(n^2)
+TC: O(n^3) and SC : O(n^2) + O(n)
 
 class Solution {
 public:
@@ -89,6 +89,46 @@ public:
         vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
 
         return func(1, n, nums, dp);
+        
+    }
+};
+
+TABULATION APPROCH:
+TC: O(n^3) and SC : O(n^2)
+
+class Solution {
+public:
+
+    int maxCoins(vector<int>& nums) {
+
+        int n = nums.size();
+
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+
+        vector<vector<int>>dp(n+2, vector<int>(n+2, 0));
+
+
+        for(int i= n; i>=1; i--){
+            for(int j = 1; j<=n; j++){
+
+                if(i>j)  continue;
+
+                int maxi = INT_MIN;
+
+                //recursive call
+                for(int ind = i; ind <= j; ind++){
+
+                    int cost = (nums[i-1] * nums[ind] * nums[j+1]) + 
+                           dp[i][ind -1] + dp[ind+1][j];
+
+                    maxi = max(maxi, cost);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+
+        return dp[1][n];
         
     }
 };
