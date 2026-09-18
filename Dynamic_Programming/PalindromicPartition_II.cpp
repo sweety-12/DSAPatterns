da4dddd1778d5202a56dfb4cc53b251907846072
Sweety Kumari <Sweetykumari.6747@gmail.com>
2026-09-19 00:51:44 +0530

    Add tabulation approach for min cut palindrome partitioning
    Implemented a tabulation approach for minimum cut palindrome partitioning with O(n^3) time complexity and O(n^2) space complexity.

--- Dynamic_Programming/PalindromicPartition_II.cpp
+++ Dynamic_Programming/PalindromicPartition_II.cpp
@@ -110,3 +110,48 @@ public:
         
     }
 };
+
+TABULATION APPROACH:
+TC: O(n^3) and SC: O(n*n)
+
+class Solution {
+public:
+    bool isPalindrome(string &s, int i, int j){
+
+        while( i<j){
+            if(s[i] != s[j])  return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    int minCut(string s) {
+
+        int n = s.length();
+
+        vector<int>dp(n+1, 0);
+
+        dp[n] = -1;
+
+        for(int i = n-1; i>=0; i--){
+
+             int mini = INT_MAX;
+
+                for(int j = i; j <n; j++){
+
+                    if(isPalindrome(s, i, j)){
+                        int cuts = 1 + dp[j+1];
+
+                        mini = min(mini, cuts);
+                    }
+                }
+
+                dp[i] = mini;
+        }
+        
+        return dp[0];
+    }
+};
